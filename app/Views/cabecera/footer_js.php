9d9a647453846e948310f3fae840e30909ba68cd
Roberto Alejandro Vásquez Alcántara <17161256@itoaxaca.edu.mx>
2022-07-19 20:16:07 -0500

    reduciendo codigo con otros archivos

---
+++ app/Views/cabecera/footer_js.php
@@ -0,0 +1,20 @@
+<script src="<?php echo base_url(); ?>/assets/src/plugins/src/global/vendors.min.js"></script>
+    <script src="<?php echo base_url(); ?>/assets/src/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="<?php echo base_url(); ?>/assets/src/plugins/src/perfect-scrollbar/perfect-scrollbar.min.js"></script>
+    <script src="<?php echo base_url(); ?>/assets/src/plugins/src/mousetrap/mousetrap.min.js"></script>
+    <script src="<?php echo base_url(); ?>/assets/layouts/vertical-dark-menu/app.js"></script>
+    <script src="<?php echo base_url(); ?>/assets/src/plugins/src/highlight/highlight.pack.js"></script>
+    <script src="<?php echo base_url(); ?>/assets/src/assets/js/custom.js"></script>
+    <!-- END GLOBAL MANDATORY SCRIPTS -->
+
+    <!-- BEGIN PAGE LEVEL CUSTOM SCRIPTS -->
+    <script src="<?php echo base_url(); ?>/assets/src/assets/js/scrollspyNav.js"></script>
+    <script>
+        checkall('checkbox_parent_all', 'checkbox_child');
+        checkall('hover_parent_all', 'hover_child');
+        checkall('striped_parent_all', 'striped_child');
+        checkall('bordered_parent_all', 'bordered_child');
+        checkall('mixed_parent_all', 'mixed_child');
+        checkall('noSpacing_parent_all', 'noSpacing_child');
+        checkall('custom_mixed_parent_all', 'custom_mixed_child');
+    </script>
